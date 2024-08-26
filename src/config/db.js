7cf37e103043fd39c26dfb3632a67c8666801b5a
AmiltonMn <amilton.moreiran@hotmail.com>
2024-08-26 11:59:39 -0300

    Commit final por enquanto

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,7 @@
 const sequelize = require('sequelize');
 
 const database = new sequelize('MML', 'MMLAdmin', 'mmladmin123456',
-{
-    dialect: 'mssql', host:'localhost', port: 56714
-}
+    {
+    dialect: 'mssql', host:'localhost', port: 51032
+    }
 )
