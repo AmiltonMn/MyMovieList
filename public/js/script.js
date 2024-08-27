1c2f0b5170c1724f9c026fe3e397f805c5c48791
AmiltonMn <amilton.moreiran@hotmail.com>
2024-08-27 09:15:15 -0300

    Construção da home com a navbar

---
+++ public/js/script.js
@@ -0,0 +1,4 @@
+/*
+Comando para instalar o NODE.JS com tudo que é necessário para rodar o site 
+npm install express mssql sequelize nodemon ejs 
+*/
--- src/config/db.js
+++ src/config/db.js
@@ -2,6 +2,10 @@ const sequelize = require('sequelize');
 
 const database = new sequelize('MML', 'MMLAdmin', 'mmladmin123456',
     {
-    dialect: 'mssql', host:'localhost', port: 51032
+        dialect: 'mssql', host:'localhost', port: 51032
+        /* 
+            Config com o banco de dados da sala original no pc do Amilton
+        */
+        //dialect: 'mssql', host:'localhost', port: 1433
     }
 )
