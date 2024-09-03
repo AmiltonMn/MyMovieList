bfeea5d81353378121d852f260547064607ed8c1
AmiltonMn <amilton.moreiran@hotmail.com>
2024-09-03 16:55:19 -0300

    Tem bastante coisa dando errado

--- public/js/script.js
+++ public/js/script.js
@@ -4,10 +4,3 @@ npm init -y
 npm install express mssql sequelize nodemon ejs
 npm install multer
 */
-
-let range = document.querySelector("#notaInput");
-let resultado = document.getElementById("resultado");
-
-function mostrarNota (nota) {
-    resultado.innerHTML = nota;
-}
