e5619be54a00c2e90106690c3c29fd6bb71848b2
Fernandohka <Fernando2005andrade@gmail.com>
2024-09-09 08:54:57 -0300

    atualizando mudança de cor ao clicar nos botoes do perfil 👍

--- public/js/script.js
+++ public/js/script.js
@@ -72,29 +72,41 @@ novaImagemFilme.addEventListener('change', () =>
 function listaFilmesClick(){
     document.getElementById("listaSeries").setAttribute("class", "collapse");
     document.getElementById("listaFilmes").setAttribute("class", "collapse show");
+    document.getElementById("botaoListaSeries").setAttribute("class", "btn btn-outline-primary d-flex align-items-end justify-content-center fs-2");
+    document.getElementById("botaoListaFilmes").setAttribute("class", "btn btn-primary d-flex align-items-end justify-content-center fs-2");
 }
 
 function listaSeriesClick(){
     document.getElementById("listaFilmes").setAttribute("class", "collapse");
     document.getElementById("listaSeries").setAttribute("class", "collapse show");
+    document.getElementById("botaoListaFilmes").setAttribute("class", "btn btn-outline-primary d-flex align-items-end justify-content-center fs-2");
+    document.getElementById("botaoListaSeries").setAttribute("class", "btn btn-primary d-flex align-items-end justify-content-center fs-2");
 }
 
 function listaFilmesAssistidosClick(){
     document.getElementById("filmesPretendoAssistir").setAttribute("class", "collapse");
     document.getElementById("filmesAssistidos").setAttribute("class", "collapse show");
+    document.getElementById("botaoPretendoAssistirFilmes").setAttribute("class", "btn btn-outline-primary");
+    document.getElementById("botaoAssistidosFilmes").setAttribute("class", "btn btn-primary");
 }
 
 function listaFilmesPretendoAssistirClick(){
     document.getElementById("filmesAssistidos").setAttribute("class", "collapse");
     document.getElementById("filmesPretendoAssistir").setAttribute("class", "collapse show");
+    document.getElementById("botaoAssistidosFilmes").setAttribute("class", "btn btn-outline-primary");
+    document.getElementById("botaoPretendoAssistirFilmes").setAttribute("class", "btn btn-primary");
 }
 
 function listaSeriesAssistidosClick(){
     document.getElementById("seriesPretendoAssistir").setAttribute("class", "collapse");
     document.getElementById("seriesAssistidos").setAttribute("class", "collapse show");
+    document.getElementById("botaoPretendoAssistirSeries").setAttribute("class", "btn btn-outline-primary");
+    document.getElementById("botaoAssistidosSeries").setAttribute("class", "btn btn-primary");
 }
 
 function listaSeriesPretendoAssistirClick(){
     document.getElementById("seriesAssistidos").setAttribute("class", "collapse");
     document.getElementById("seriesPretendoAssistir").setAttribute("class", "collapse show");
+    document.getElementById("botaoAssistidosSeries").setAttribute("class", "btn btn-outline-primary");
+    document.getElementById("botaoPretendoAssistirSeries").setAttribute("class", "btn btn-primary");
 }
