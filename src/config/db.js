afa411ecc6e610b78ff50c2d1e3b06b600b804e8
AmiltonMn <amilton.moreiran@hotmail.com>
2024-09-11 19:17:35 -0300

    Teste 3

--- src/config/db.js
+++ src/config/db.js
@@ -1,21 +1,22 @@
 require('dotenv').config();
 const Sequelize = require('sequelize');
 
-// Verifica se está em produção ou desenvolvimento (localhost)
-const isProduction = process.env.NODE_ENV === 'production';
+// Pega a URL do banco de dados do ambiente
+const databaseUrl = process.env.DATABASE_URL;
 
-// Se estiver em produção, utiliza a URL do banco remoto, senão usa o localhost
-const databaseUrl = isProduction ? process.env.DATABASE_URL : process.env.LOCAL_DATABASE_URL;
+if (!databaseUrl) {
+    throw new Error("A variável de ambiente DATABASE_URL não está definida.");
+}
 
 const database = new Sequelize(databaseUrl, {
     dialect: 'postgres',
     dialectOptions: {
-        ssl: isProduction ? { require: true, rejectUnauthorized: false } : false, // SSL apenas em produção
+        ssl: true, // Caso o provedor precise de SSL
     }
 });
 
 database.sync()
-    .then(() => console.log("Conectado com sucesso ao banco de dados"))
+    .then(() => console.log("Conectado com sucesso ao banco de dados PostgreSQL"))
     .catch(err => console.error("Erro ao conectar ao banco de dados:", err));
 
 module.exports = database;
