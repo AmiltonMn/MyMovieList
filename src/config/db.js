4adc2d37db6e2795f64343251bff7302fa7f1b7f
Fernandohka <Fernando2005andrade@gmail.com>
2024-09-11 14:38:51 -0300

    icones home series

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ const sequelize = require('sequelize');
 
 const database = new sequelize('MML', 'MMLAdmin', 'mmladmin123456',
     {
-        dialect: 'mssql', host:'localhost', port: 51032
+        // dialect: 'mssql', host:'localhost', port: 51032
 
         // dialect: 'mssql', host:'localhost', port: 56732
 
